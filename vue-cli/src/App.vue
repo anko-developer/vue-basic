08be251db193a925d0024e5a5332a2897c65172f
김명욱 <anko@gsitm.com>
2022-05-24 09:59:23 +0900

    vue-cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <AppHeader v-bind:propsdata="str" v-on:renew="renewStr" />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      str: 'Header1'
+    }
+  },
+  components: {
+    AppHeader
+  },
+  methods: {
+    renewStr() {
+      this.str = 'wow';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
